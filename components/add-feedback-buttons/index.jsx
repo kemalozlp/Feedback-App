e826f22d13db9145bfca707d930560bb6fef584c
Furkan <furkancosar2005@gmail.com>
2024-10-29 00:58:16 +0300

    feedback button componenti oluşturuldu

---
+++ components/add-feedback-buttons/index.jsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useState } from 'react';
+import "./add-feedback-button.css"
+import NewFeedbackForm from '../new-feedback-modal';
+import { Less } from '@/helpers/icons';
+
+export default function AddFeedbackButton() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    return (
+        <div>
+            <button  className="add-feedback-button" onClick={openModal}>+ Add Feedback</button>
+
+            {isModalOpen && 
+            (
+              <div className="modal">
+                <div className="modal-content">
+                  <button className="back-button" onClick={closeModal}>
+                    <Less />
+                    Go Back 
+                  </button>
+
+                  <NewFeedbackForm />
+                </div>
+              </div>
+            )}
+        </div>
+    )
+}
