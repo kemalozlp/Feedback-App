ed8d289d5238740ec0fc68d0deeae8cf21462ad3
Furkan <furkancosar2005@gmail.com>
2024-10-29 01:06:48 +0300

    frontend mentor box componenti oluşturuldu

---
+++ components/sidebar/frontend-mentor-box/index.jsx
@@ -0,0 +1,10 @@
+export default function FrontendMentorBox() {
+  return (
+    <div className="frontend-mentor">
+      <div className="texts">
+        <h3>Frontend Mentor</h3>
+        <p>Feedback Board</p>
+      </div>
+    </div>
+  );
+}
