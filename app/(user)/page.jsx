02a886437c598df4bdc381a84439c88fd9c732d8
Furkan <furkancosar2005@gmail.com>
2024-10-29 00:55:44 +0300

    layout eklendi

--- app/(user)/page.jsx
+++ app/(user)/page.jsx
@@ -1,16 +1,34 @@
+import Image from "next/image";
+import Header from "@/components/header";
+import AddFeedbackButton from "@/components/add-feedback-buttons";
+import SideBar from "@/components/sidebar";
 import "./user-home.css";
+import Card from "@/components/feedback-card";
+import Link from "next/link";
 
 export default function UserHome() {
     return (
         <div className="container">
+            <SideBar />
+
+            <div className="main">
+                <Header />
+
                 <div className="empty-container">
-                    <Image src="../image/empty-image.png" alt="" />
+                    <Image width={130} height={136} src="/image/bosmes.png" alt="" />
 
                     <div className="empty-texts">
                         <h3>There is no feedback yet.</h3>
                         <p>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
                     </div>
+
+                    <AddFeedbackButton />
                 </div>
+                
+                <Link className="card-link"  href={`/${1}`}>                
+                    <Card />
+                </Link>
             </div>
+        </div>
     )
 }
