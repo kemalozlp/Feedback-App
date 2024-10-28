04a177065103ce19a0e3bfd6b0d7afce1dc3b5d5
Furkan <furkancosar2005@gmail.com>
2024-10-29 01:06:31 +0300

    category box componenti oluşturuldu

---
+++ components/sidebar/category-box/index.jsx
@@ -0,0 +1,12 @@
+export default function CategoryBox() {
+    return (
+        <div className="categories">
+            <button className="category-all">All</button>
+            <button className="category-item">UI</button>
+            <button className="category-item">UX</button>
+            <button className="category-item">Enhancement</button>
+            <button className="category-item">Bug</button>
+            <button className="category-item">Feature</button>
+        </div>
+    )
+}
