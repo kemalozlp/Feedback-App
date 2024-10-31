07a361ac7356e2c4625421a281591a1b46897392
Furkan <furkancosar2005@gmail.com>
2024-10-31 14:40:40 +0300

    home page mobile responsive eklendi

--- app/(user)/page.jsx
+++ app/(user)/page.jsx
@@ -7,28 +7,31 @@ import Card from "@/components/feedback-card";
 import Link from "next/link";
 
 export default function UserHome() {
-    return (
-        <div className="container">
-            <SideBar />
+  return (
+    <div className="container">
+      <SideBar />
 
-            <div className="main">
-                <Header />
+      <div className="main">
+        <Header />
 
-                <div className="empty-container">
-                    <Image width={130} height={136} src="/image/bosmes.png" alt="" />
+        <div className="empty-container">
+          <Image width={130} height={136} src="/image/bosmes.png" alt="" />
 
-                    <div className="empty-texts">
-                        <h3>There is no feedback yet.</h3>
-                        <p>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
-                    </div>
+          <div className="empty-texts">
+            <h3>There is no feedback yet.</h3>
+            <p>
+              Got a suggestion? Found a bug that needs to be squashed? We love
+              hearing about new ideas to improve our app.
+            </p>
+          </div>
 
-                    <AddFeedbackButton />
-                </div>
-                
-                <Link className="card-link"  href={`/${1}`}>                
-                    <Card />
-                </Link>
-            </div>
+          <AddFeedbackButton />
         </div>
-    )
+
+        <div className="card-lists">
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
 }
