d886dc1eda1a257269cd80b7eb5692fe6d0ff1ca
Furkan <furkancosar2005@gmail.com>
2024-10-31 14:43:01 +0300

    feedback card mobile responsive eklendi

--- components/feedback-card/index.jsx
+++ components/feedback-card/index.jsx
@@ -1,6 +1,7 @@
 import { Comments } from "@/helpers/icons";
 import LikeButton from "../feedback-like";
 import "./feedback-card.css";
+import Link from "next/link";
 
 export default function Card() {
   return (
@@ -11,7 +12,9 @@ export default function Card() {
         </div>
 
         <div className="card-texts">
-          <h4>Add tags for solutions</h4>
+          <Link className="card-link"  href={`/${1}`}>          
+            <h4>Add tags for solutions</h4>
+          </Link>
           <p>Easier to search for solutions based on a specific stack.</p>
           <p className="card-category">Enhancement</p>
         </div>
@@ -20,6 +23,15 @@ export default function Card() {
       <div className="comments">
         <Comments />
         <p>2</p>
+      </div>
+
+      <div className="like-and-comments-mobile">
+        <LikeButton />
+
+        <div className="comments-mobile">
+          <Comments />
+          <p>2</p>
+        </div>
       </div>
     </div>
   );
