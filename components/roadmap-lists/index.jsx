337bbceed33d30794e103ffe9b94301a72b862b3
Furkan <furkancosar2005@gmail.com>
2024-11-07 23:06:58 +0300

    roadmap sayfası maple dönüldü

--- components/roadmap-lists/index.jsx
+++ components/roadmap-lists/index.jsx
@@ -5,10 +5,15 @@ import { CommentsIcon, UpVoteIcon } from "@/helpers/icons";
 import "./roadmap-lists.css";
 import Link from "next/link";
 
-export default function RoadmapLists() {
+export default function RoadmapLists({ data }) {
   const [likes, setLikes] = useState(112);
   const [liked, setLiked] = useState(false);
   const [situation, setSituation] = useState("progress");
+
+  const plannedData = data.filter((item) => item.status === 1);
+  const progressData = data.filter((item) => item.status === 2);
+  const livedData = data.filter((item) => item.status === 3);
+  console.log(plannedData);
 
   const handleLike = () => {
     if (liked) {
@@ -46,173 +51,136 @@ export default function RoadmapLists() {
         <div className="planned">
           <div className="planned-texts">
             <h3>
-              Planned <span>{"(1)"}</span>
+              Planned <span>{`(${plannedData.length})`}</span>
             </h3>
             <p>Ideas prioritized for research</p>
           </div>
 
-          <div className="roadmap-card-lists">
-            <div className="roadmap-card">
-              <div className="status">
-                <p>
-                  <span>●</span>Planned
-                </p>
-              </div>
+          {plannedData.map((x, i) => (
+            <div className="roadmap-card-lists" key={i}>
+              <div className="roadmap-card">
+                <div className="status">
+                  <p>
+                    <span>●</span>Planned
+                  </p>
+                </div>
 
-              <div className="card-texts">
-                <Link href={`/${1}`} className="card-detail">
-                  More comprehensive reports
-                </Link>
-                <p>
-                  It would be great to see a more detailed breakdown of
-                  solutions.
-                </p>
-              </div>
+                <div className="card-texts">
+                  <Link href={`/${1}`} className="card-detail">
+                    {x.title}
+                  </Link>
+                  <p>{x.detail}</p>
+                </div>
 
-              <p className="card-category">Feature</p>
+                <p className="card-category">{x.category}</p>
 
-              <div className="like-and-comments">
-                <button
-                  className={liked === true ? "likes select-btn" : "likes"}
-                  onClick={handleLike}
-                >
-                  <UpVoteIcon />
-                  <p>{likes}</p>
-                </button>
+                <div className="like-and-comments">
+                  <button
+                    className={liked === true ? "likes select-btn" : "likes"}
+                    onClick={handleLike}
+                  >
+                    <UpVoteIcon />
+                    <p>{x.commentCount}</p>
+                  </button>
 
-                <div className="comments">
-                  <CommentsIcon />
-                  <p>2</p>
+                  <div className="comments">
+                    <CommentsIcon />
+                    <p>{x.commentCount}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
-        
+
         <div className="in-progress">
           <div className="in-progress-texts">
             <h3>
-              In-Progress <span>{"(2)"}</span>
+              In-Progress <span>{`(${progressData.length})`}</span>
             </h3>
             <p>Currently being developed</p>
           </div>
 
-          <div className="roadmap-card-lists">
-            <div className="roadmap-card">
-              <div className="status">
-                <p>
-                  <span>●</span>In Progress
-                </p>
-              </div>
+          {progressData.map((x, i) => (
+            <div className="roadmap-card-lists" key={i}>
+              <div className="roadmap-card">
+                <div className="status">
+                  <p>
+                    <span>●</span>In Progress
+                  </p>
+                </div>
 
-              <div className="card-texts">
-                <Link href={`/${1}`} className="card-detail">
-                  One-click portfolio generation
-                </Link>
-                <p>
-                  Add ability to create professional looking portfolio from
-                  profile.
-                </p>
-              </div>
+                <div className="card-texts">
+                  <Link href={`/${1}`} className="card-detail">
+                    {x.title}
+                  </Link>
+                  <p>{x.detail}</p>
+                </div>
 
-              <p className="card-category">Feature</p>
+                <p className="card-category">{x.category}</p>
 
-              <div className="like-and-comments">
-                <button
-                  className={liked === true ? "likes select-btn" : "likes"}
-                  onClick={handleLike}
-                >
-                  <UpVoteIcon />
-                  <p>{likes}</p>
-                </button>
+                <div className="like-and-comments">
+                  <button
+                    className={liked === true ? "likes select-btn" : "likes"}
+                    onClick={handleLike}
+                  >
+                    <UpVoteIcon />
+                    <p>{x.voteCount}</p>
+                  </button>
 
-                <div className="comments">
-                  <CommentsIcon />
-                  <p>2</p>
+                  <div className="comments">
+                    <CommentsIcon />
+                    <p>{x.commentCount}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="roadmap-card-lists">
-            <div className="roadmap-card">
-              <div className="status">
-                <p>
-                  <span>●</span>In Progress
-                </p>
-              </div>
-
-              <div className="card-texts">
-                <Link href={`/${1}`} className="card-detail">
-                  One-click portfolio generation
-                </Link>
-                <p>
-                  Add ability to create professional looking portfolio from
-                  profile.
-                </p>
-              </div>
-
-              <p className="card-category">Feature</p>
-
-              <div className="like-and-comments">
-                <button
-                  className={liked === true ? "likes select-btn" : "likes"}
-                  onClick={handleLike}
-                >
-                  <UpVoteIcon />
-                  <p>{likes}</p>
-                </button>
-
-                <div className="comments">
-                  <CommentsIcon />
-                  <p>2</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
 
         <div className="live">
           <div className="live-texts">
             <h3>
-              Live <span>{"(1)"}</span>
+              Live <span>{`(${livedData.length})`}</span>
             </h3>
             <p>Released features</p>
           </div>
 
-          <div className="roadmap-card-lists">
-            <div className="roadmap-card">
-              <div className="status">
-                <p>
-                  <span>●</span>Live
-                </p>
-              </div>
+          {livedData.map((x, i) => (
+            <div className="roadmap-card-lists">
+              <div className="roadmap-card">
+                <div className="status">
+                  <p>
+                    <span>●</span>Live
+                  </p>
+                </div>
 
-              <div className="card-texts">
-                <Link href={`/${1}`} className="card-detail">
-                  Add micro-interactions
-                </Link>
-                <p>Small animations at specific points can add delight.</p>
-              </div>
+                <div className="card-texts">
+                  <Link href={`/${1}`} className="card-detail">
+                    {x.title}
+                  </Link>
+                  <p>{x.detail}</p>
+                </div>
 
-              <p className="card-category">Feature</p>
+                <p className="card-category">{x.category}</p>
 
-              <div className="like-and-comments">
-                <button
-                  className={liked === true ? "likes select-btn" : "likes"}
-                  onClick={handleLike}
-                >
-                  <UpVoteIcon />
-                  <p>{likes}</p>
-                </button>
+                <div className="like-and-comments">
+                  <button
+                    className={liked === true ? "likes select-btn" : "likes"}
+                    onClick={handleLike}
+                  >
+                    <UpVoteIcon />
+                    <p>{x.voteCount}</p>
+                  </button>
 
-                <div className="comments">
-                  <CommentsIcon />
-                  <p>2</p>
+                  <div className="comments">
+                    <CommentsIcon />
+                    <p>{x.commentCount}</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
