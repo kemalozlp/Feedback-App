488082de27a4fc8281d48f0c0fa3818539d83cea
Furkan <furkancosar2005@gmail.com>
2024-11-09 10:34:45 +0300

    like butonu, hamburger menu düzenlendi

--- components/sidebar/category-box/index.jsx
+++ components/sidebar/category-box/index.jsx
@@ -2,8 +2,8 @@ export default function CategoryBox({categories}) {
     return (
       <div className="categories">
         <button className="category-all">All</button>
-        {categories?.map(x => (
-          <button className="category-item">{x}</button>
+        {categories?.map((x, i) => (
+          <button className="category-item" key={i}>{x}</button>
         ))}
       </div>
     );
