c2bd4e7f71dbd6643807e32d8309deb6f0d4f799
Furkan <furkancosar2005@gmail.com>
2024-11-09 10:34:02 +0300

    login düzenlendi

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -2,15 +2,20 @@
 
 import { loginUser } from "@/components/login/action.jsx";
 import { useFormState } from "react-dom";
+import "./login.css";
+import Link from "next/link";
 
 export default function Login() {
   const [state, action] = useFormState(loginUser, null);
   return (
     <>
       {state?.error && <div>{state.error}</div>}
-      <form action={action}>
-        <input type="email" name="email" placeholder="E-posta Adresi" /> <br />
+      <Link href={"/"}>Geri Dön</Link>
+
+      <form action={action} className="loginForm">
+        <input type="email" name="email" placeholder="E-posta Adresi" className="email"/>
         <input type="password" name="password" placeholder="Şifre" /> <br />
+
         <button>Giriş Yap</button>
       </form>
     </>
